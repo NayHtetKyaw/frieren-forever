fa6f5ba47ddef5c0f57e2f08bec930a258361bb6
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-04-25 14:47:38 +0700

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,9 @@ export default function Home() {
   return (
     <Container className="h-full">
       <Section>
-        <Heading className="flex justify-center h-96">
+        <Heading className="flex flex-col justify-center h-96">
           <span className="sm:text-xl md:text-2xl lg:text-5xl font-bold self-center"> 葬送のフリーレン</span>
+          <span className="sm:text-lg md:text-xl lg:text-2xl font-bold self-center"> Beyound Journey's End</span>
         </Heading>
         <BackgroundBeams />
       </Section>
