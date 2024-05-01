385eccb9a766cfe707cd0bcd7245b5dd3c3ce576
surattinon <surattinon.basu@gmail.com>
2024-05-01 23:36:03 +0700

    remove pic and map func

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -2,20 +2,6 @@ import React from "react";
 import Image from "next/image";
 import Profile from "@/components/profile";
 
-const pic = ["/aura_profile.png", "/Aura_BG.png"];
-
-const allPic = pic.map((pics) => (
-  <div key={pic.length}>
-    <Profile
-      className="rounded-md"
-      src={pics}
-      width={200}
-      height={200}
-      alt="Picture of the author"
-    />
-  </div>
-));
-
 function page() {
   return (
     // Characters contents
@@ -27,7 +13,7 @@ function page() {
       {/* Character Cards */}
       <div className=" px-10 py-5 flex flex-col justify-center">
         {/* Aura Card */}
-        <div className="flex flex-row flex-wrap justify-center ">{allPic}</div>
+        <Profile />
       </div>
     </section>
   );
